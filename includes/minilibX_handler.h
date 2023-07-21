566b9a746e4ebb3400743cbc59c8b4d2663770ff
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-07-21 18:20:33 +0400

    WIP

--- includes/minilibX_handler.h
+++ includes/minilibX_handler.h
@@ -7,6 +7,7 @@ void	mlx_hooks_handler(t_data *data);
 int	ft_exit(int keycode, void *f);
 int	press_exit(int keycode, void *f);
 int	mouse_whilling(int button, int x, int y, t_data *data);
+int	press_object_movement_keys(int keycode, t_data *data);
 
 
 #endif //MINIRT_MINILIBX_HANDLER_H
