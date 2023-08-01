5f468bf683f07b68b4dc58e449fdc15af3abbc36
karamon17 <gevorik18@gmail.com>
2023-08-01 19:30:45 +0400

    WIP

--- sources/main/light.c
+++ sources/main/light.c
@@ -6,33 +6,27 @@
 /*   By: gkhaishb <gkhaishb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 16:16:22 by gkhaishb          #+#    #+#             */
-/*   Updated: 2023/08/01 18:42:31 by gkhaishb         ###   ########.fr       */
+/*   Updated: 2023/08/01 20:34:53 by gkhaishb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/miniRT.h"
 
-float	ft_specular(t_vector l, t_vectors *input, t_light *cur, t_abc *abc)
+float	ft_specular(t_vector l, t_vectors *input, t_light *cur, t_abc abc)
 {
 	t_vector	r;
 	float		res;
 
 	res = 0;
-	if (abc->b != -1)
+	if (abc.b != -1)
 	{
 		r = vector_subtract(mult_vect(2 * dot(input->v2, l), \
 			input->v2), l);
-		abc->c = dot(r, input->v4);
-		// if (round(pow(abc->c / (vect_len(r) \
-		// 	* vect_len(input->v4)), abc->b)) != 0)
-		// 	printf("%f\n", pow(abc->c / (vect_len(r) \
-		// 		* vect_len(input->v4)), abc->b));
-		if (abc->c > 0)
-			res = cur->intensity * pow(abc->c / (vect_len(r) \
-			* vect_len(input->v4)), abc->b);
+		abc.c = dot(r, input->v4);
+		if (abc.c > 0)
+			res = cur->intensity * pow(abc.c / (vect_len(r) \
+			* vect_len(input->v4)), abc.b);
 	}
-	if (round(res) != 0)
-		printf ("%f\n", res);
 	return (res);
 }
 
@@ -60,7 +54,7 @@ float	compute_lighting(t_data *data, t_vectors *input, float s)
 				abc.a += cur->intensity * dot(input->v2, l) / \
 				(vect_len(input->v2) * vect_len(l));
 			abc.b = s;
-			abc.a += ft_specular(l, input, cur, &abc);
+			abc.a += ft_specular(l, input, cur, abc);
 		}
 		cur = cur->next;
 	}
